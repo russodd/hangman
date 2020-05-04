a0246375ddb5579d6c8eb9d5de2b491ff6ceab07
Daniel Russo <russodd@daniel-russos-computer.local>
2020-05-03 17:40:09 -0700

    Added support for guess endpoint

--- api/routes/hangman.js
+++ api/routes/hangman.js
@@ -14,7 +14,6 @@ const GAME_STATE_TEMPLATE = {   // A template for a new game state (representing
     "incorrectGuesses": [], // The letters the user has guessed incorrectly
     "guessesLeft": GUESSES_PER_GAME,   // The number of incorrect guesses the player has left
     "gameComplete": false,  // A flag for whether the current game is in a completed state
-    "gameLost": false, // A flag saying whether the player has lost the game
     "hasWordsRemaining": true,  // A flag showing whether there are any more words left to guess at after the current one
     "prevMove": null    // The game state ID of the player's previous move, or null when the player is starting a brand-new game
 };
@@ -68,9 +67,7 @@ function startNewGame(prevState) {
 
     // Create an MD5 hash to be used as a game state ID
     newGameState.id = crypto.createHash('md5').update(JSON.stringify(newGameState)).digest('hex');
-
-    // Add the new game state to the dictionary of game states
-    gameStates[newGameState.id] = newGameState;
+    console.log('New game ID: '+newGameState.id);
 
     return newGameState;
 }
@@ -84,13 +81,14 @@ function stripHiddenFields(gameState) {
         incorrectGuesses: gameState.incorrectGuesses,
         guessesLeft: gameState.guessesLeft,
         gameComplete: gameState.gameComplete,
-        gameLost: gameState.gameLost,
         hasWordsRemaining: gameState.hasWordsRemaining,
         prevMove: gameState.prevMove
     };
 }
 
 // Load hangman game state data from a given ID
+// Arguments:
+// state - a valid game state ID
 router.get('/load', function(req, res, next) {
     if (req.query.state) {
         const gameState = gameStates[req.query.state];
@@ -101,8 +99,96 @@ router.get('/load', function(req, res, next) {
         }
     } else {
         const gameState = startNewGame();
+        // Add the new game state to the dictionary of game states
+        gameStates[gameState.id] = gameState;
+        // Return the game state
         res.send(stripHiddenFields(gameState));
     }
+});
+
+// Load the result of a guess, or of creating a new game (if no guess is given and the current game state is an endgame)
+// Arguments:
+// state - a valid game state ID
+// guess - a single character, or a string representing a whole word guess
+router.get('/guess', function(req, res, next) {
+    const gameState = req.query.state ? gameStates[req.query.state] : null;
+    // Normalize guesses to all caps
+    const guess = req.query.guess ? req.query.guess.toUpperCase() : null;
+
+    if (!gameState) {
+        throw new Error('Invalid game state: ' + req.query.state);
+    }
+
+    let newGameState = null;
+
+    if (gameState.guessesLeft === 0) {
+        throw new Error ('No guesses remaining');
+    }
+
+    if (gameState.gameComplete === true) {
+        // A new game
+        if (!gameState.hasWordsRemaining) {
+            throw new Error('There are no words remaining');
+        }
+        newGameState = startNewGame(gameState.id);
+    } else {
+        if (!guess) {
+            throw new Error('No guess argument found');
+        }
+        // Make a deep copy of the current game state
+        newGameState = JSON.parse(JSON.stringify(gameState));
+        // Strip out its ID
+        newGameState.id = undefined;
+        if (guess.length === 1) {
+            // A letter guess
+            if (gameState.letterLookup[guess]) {
+                // A correct guess
+                newGameState.letterLookup[guess].forEach(pos => {
+                    newGameState.letters[pos] = guess;
+                });
+                if (newGameState.letters.indexOf('_') < 0) {
+                    newGameState.gameComplete = true;
+                }if (newGameState.wordsRemaining.length === 0) {
+                    newGameState.hasWordsRemaining = false;
+                }
+            } else {
+                // An incorrect guess
+                newGameState.incorrectGuesses.push(guess);
+                newGameState.guessesLeft--;
+                if (newGameState.guessesLeft === 0) {
+                    newGameState.gameComplete = true;
+                }
+            }
+        } else {
+            // A whole word guess
+            let correct = true;
+            // Check that every letter in the guess is where it should be
+            guess.split('').forEach((letter, idx) => {
+                if (newGameState.letterLookup[letter].indexOf(idx) < 0) {
+                    correct = false;
+                }
+            });
+            // Regardless of whether the guess is correct or incorrect, the game will be complete
+            newGameState.gameComplete = true;
+            if (correct) {
+                // The word was guessed correctly
+                guess.split('').forEach((letter, idx) => {
+                    newGameState.letters[idx] = letter;
+                });
+            } else {
+                // The word was guessed incorrectly
+                newGameState.incorrectGuesses.push(guess);
+                newGameState.guessesLeft = 0;
+            }
+        }
+        // Create an MD5 hash to be used as a game state ID
+        newGameState.id = crypto.createHash('md5').update(JSON.stringify(newGameState)).digest('hex');
+    }
+
+    // Add the new game state to the dictionary of game states
+    gameStates[newGameState.id] = newGameState;
+    // Return the new game state
+    res.send(stripHiddenFields(newGameState));
 });
 
 module.exports = router;
--- api/routes/index.js
+++ api/routes/index.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/load', function(req, res, next) {
-  res.send({
-    "id": 0, // an integer representing the current game state, which can be added to the client URL in order to call up a particular game state at any time.
-    "letters": ["_", "_", "_", "_", "_", "_"],  // The correctly-guessed letters of the word, with blanks representing letters that havent been found yet
-    "guessesLeft": 8,   // The number of incorrect guesses the player has left
-    "gameComplete": false,  // A flag for whether the current game is in a completed state
-    "gameLost": false, // A flag saying whether the player has lost the game
-    "wordsRemaining": true,  // A flag showing whether there are any more words left to guess at after the current one
-    "prevMove": null    // The game state ID of the player's previous move, or null when the player is starting a brand-new game
-  });
-});
-
 module.exports = router;
